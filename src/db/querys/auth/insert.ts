1c79a85ea3d5edb3a2438c4baf744679c2d44ba8
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2026-04-04 12:04:52 -0300

    refactor: cleaning the typing of inserting the usiario and changing the name of passHash

--- src/db/querys/auth/insert.ts
+++ src/db/querys/auth/insert.ts
@@ -1,20 +1,15 @@
 import { db } from "@/db/client";
 import { users } from "@/db/schema/user";
 
-type InsertUser = {
-  id: string
-  name: string
-  email: string
-  pass: string
-}
+type InsertUser = typeof users.$inferInsert
 
-export async function insertUser({ id, name, email, pass }: InsertUser) {
+export async function insertUser({ id, name, email, passHash }: InsertUser) {
  try {
    const [result] = await db.insert(users).values({
      id,
      name,
      email,
-     passHash: pass,
+     passHash,
    }).returning({ id: users.id })
 
    return result?.id ?? null
